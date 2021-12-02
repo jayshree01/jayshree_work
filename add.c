862e8d8c4ed7beeaaa343a1e328947344fff8ad4
jayshree01 <jayu9399@gmail.com>
2021-12-03 04:37:24 +0530

    jayu commit

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  int main()
+  {
+    int a ,b,c;
+    printf("Enter a and b \n");
+
+     scanf("%d%d",&a,&b);
+     c=a+b;
+     printf("Addition %d",c);
+    
+      
+    return 0;
+  }
